332a5c3f0b0d65ad8cff8168918841bc64c32a4a
Yuval Marcus <ymarcus93@gmail.com>
2022-06-30 10:56:37 -0400

    Cleanup

--- bctl/agent/keysplitting/keysplitting_test.go
+++ bctl/agent/keysplitting/keysplitting_test.go
@@ -298,10 +298,6 @@ var _ = Describe("Agent keysplitting", func() {
 				CommonAssertBehavior()
 			})
 		})
-
-		// Describe("the failure path", func() {
-
-		// })
 	})
 
 	Describe("validate daemon messages", func() {
