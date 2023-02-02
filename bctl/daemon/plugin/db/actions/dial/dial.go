318340410e61a30e0106e331e404f95203ab2170
johncmerfeld <john.merfeld@gmail.com>
2023-02-02 14:20:40 -0500

    trying to fix dial disconnects

--- bctl/daemon/plugin/db/actions/dial/dial.go
+++ bctl/daemon/plugin/db/actions/dial/dial.go
@@ -81,7 +81,7 @@ func (d *DialAction) Start(lconn *net.TCPConn) error {
 			for {
 				if n, err := lconn.Read(buf); !d.tmb.Alive() {
 					return nil
-				} else if n == 0 {
+				} else if n == 0 && err != io.EOF {
 					continue
 				} else if err != nil {
 					// print our error message
