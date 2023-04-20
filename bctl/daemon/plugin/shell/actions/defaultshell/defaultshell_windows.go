5bfd8d87ee4a0bf73fe906be055aed7c1eab2dfe
lgmugnier <mugnier@bu.edu>
2023-04-20 13:16:38 -0400

    Fix for windows daemon build (#236)

--- bctl/daemon/plugin/shell/actions/defaultshell/defaultshell_windows.go
+++ bctl/daemon/plugin/shell/actions/defaultshell/defaultshell_windows.go
@@ -18,7 +18,7 @@ func (d *DefaultShell) start(attach bool) error {
 		defer d.logger.Infof("closing action: %s", d.tmb.Err())
 		defer current.Reset()
 
-		d.tmb.Go(d.listenForTerminalSizeChanges(current))
+		d.tmb.Go(func() error { return d.listenForTerminalSizeChanges(current) })
 
 		// switch our terminal into 'raw' mode
 		if err := current.SetRaw(); err != nil {
@@ -36,7 +36,7 @@ func (d *DefaultShell) listenForTerminalSizeChanges(console console.Console) err
 	// Send our initial size
 	oldSize, err := console.Size()
 	if err != nil {
-		return fmt.Errorf("Failed to get current terminal size %s", err)
+		return fmt.Errorf("failed to get current terminal size %s", err)
 	}
 
 	shellResizeMessage := bzshell.ShellResizeMessage{
@@ -46,25 +46,23 @@ func (d *DefaultShell) listenForTerminalSizeChanges(console console.Console) err
 	d.sendOutputMessage(bzshell.ShellResize, shellResizeMessage)
 
 	// Occasionally polls the terminal size to check for changes and sends any it detects
-	go func() {
-		for {
-			select {
-			case <-d.tmb.Dying():
-				return
-			case <-time.After(50 * time.Millisecond):
-				if newSize, err := console.Size(); err != nil {
-					d.logger.Errorf("Failed to get current terminal size %s", err)
-				} else if newSize.Width != oldSize.Width || newSize.Height != oldSize.Height {
+	for {
+		select {
+		case <-d.tmb.Dying():
+			return nil
+		case <-time.After(50 * time.Millisecond):
+			if newSize, err := console.Size(); err != nil {
+				d.logger.Errorf("Failed to get current terminal size %s", err)
+			} else if newSize.Width != oldSize.Width || newSize.Height != oldSize.Height {
 
-					shellResizeMessage := bzshell.ShellResizeMessage{
-						Rows: uint32(newSize.Height),
-						Cols: uint32(newSize.Width),
-					}
-					d.sendOutputMessage(bzshell.ShellResize, shellResizeMessage)
-
-					oldSize = newSize
+				shellResizeMessage := bzshell.ShellResizeMessage{
+					Rows: uint32(newSize.Height),
+					Cols: uint32(newSize.Width),
 				}
+				d.sendOutputMessage(bzshell.ShellResize, shellResizeMessage)
+
+				oldSize = newSize
 			}
 		}
-	}()
+	}
 }
